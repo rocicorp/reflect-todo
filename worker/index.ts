d3a01bc5f3d625ec4bbc9cc75d5c7eadf3e5802f
Aaron Boodman <aaron@aaronboodman.com>
2023-02-16 02:20:47 -1000

    Simplify logging and metrics

--- worker/index.ts
+++ worker/index.ts
@@ -1,4 +1,10 @@
-import { createReflectServer } from "@rocicorp/reflect-server";
+import {
+  consoleLogSink,
+  createReflectServer,
+  DatadogLogSink,
+  DatadogMetricsSink,
+  ReflectServerBaseEnv,
+} from "@rocicorp/reflect-server";
 import { mutators } from "../src/mutators";
 
 const authHandler = async (auth: string) => {
@@ -7,9 +13,31 @@ const authHandler = async (auth: string) => {
   };
 };
 
+type Env = ReflectServerBaseEnv & {
+  ["DATADOG_API_KEY"]?: string | undefined;
+};
+
 const { worker, RoomDO, AuthDO } = createReflectServer({
   mutators,
   authHandler,
   getLogLevel: () => "debug",
+  getLogSinks: (env: Env) => {
+    const sinks = [consoleLogSink];
+    if (env.DATADOG_API_KEY) {
+      sinks.push(
+        new DatadogLogSink({
+          apiKey: env.DATADOG_API_KEY,
+          service: "replidraw-do",
+        })
+      );
+    }
+    return sinks;
+  },
+  getMetricsSink: (env: Env) =>
+    env.DATADOG_API_KEY
+      ? new DatadogMetricsSink({
+          apiKey: env.DATADOG_API_KEY,
+        })
+      : undefined,
 });
 export { worker as default, RoomDO, AuthDO };
