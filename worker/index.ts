99f87267d30a7256b3d2568e351686f62a9582a8
Aaron Boodman <aaron@aaronboodman.com>
2023-05-04 14:59:43 -1000

    test metrics integration

--- worker/index.ts
+++ worker/index.ts
@@ -1,9 +1,16 @@
-import { createReflectServer } from "@rocicorp/reflect-server";
+import {
+  createDatadogMetricsSink,
+  createReflectServer,
+} from "@rocicorp/reflect-server";
 import { mutators } from "../src/mutators";
 
 const { worker, RoomDO, AuthDO } = createReflectServer({
   mutators,
   logLevel: "debug",
+  metricsSink: createDatadogMetricsSink({
+    apiKey: "58066c1d3098ad9cb0bb30bac696cc08",
+    service: "aa-reflect-todo",
+  }),
 });
 
 export { worker as default, RoomDO, AuthDO };
