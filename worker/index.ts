fb17c19c780c99cd6d4b93f3e266249f00819960
Aaron Boodman <aaron@aaronboodman.com>
2023-05-24 08:55:14 -1000

    Whoops, update call signature of createReflectServer

--- worker/index.ts
+++ worker/index.ts
@@ -1,9 +1,9 @@
 import { createReflectServer } from "@rocicorp/reflect-server";
 import { mutators } from "../src/mutators";
 
-const { worker, RoomDO, AuthDO } = createReflectServer({
+const { worker, RoomDO, AuthDO } = createReflectServer(() => ({
   mutators,
   logLevel: "debug",
-});
+}));
 
 export { worker as default, RoomDO, AuthDO };
