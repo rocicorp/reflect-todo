9cb9ca3ea812fe10dded30196ebaebc9e87e0dfe
Aaron Boodman <aaron@aaronboodman.com>
2023-08-23 23:31:56 -1000

    Port to Mirror :)

--- worker/index.ts
+++
@@ -1,9 +0,0 @@
-import { createReflectServer } from "@rocicorp/reflect/server";
-import { mutators } from "../src/mutators";
-
-const { worker, RoomDO, AuthDO } = createReflectServer(() => ({
-  mutators,
-  logLevel: "debug",
-}));
-
-export { worker as default, RoomDO, AuthDO };
