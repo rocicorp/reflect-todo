fca1fe87d14ae24e47a6633f1eca2ade63be7a53
Aaron Boodman <aaron@aaronboodman.com>
2024-02-01 15:52:47 -1000

    Do cd/ci from Vercel instead.

--- src/host.ts
+++ src/host.ts
@@ -1,12 +1,5 @@
-export const reflectServer = import.meta.env.VITE_REFLECT_SERVER
-  ? applyTemplate(import.meta.env.VITE_REFLECT_SERVER)
-  : "";
+export const reflectServer = import.meta.env.VITE_REFLECT_SERVER!;
 
-function applyTemplate(template: string) {
-  const f = new Function(
-    "VITE_VERCEL_GIT_COMMIT_REF",
-    `return \`${template}\``
-  );
-  const branchName = import.meta.env.VITE_VERCEL_GIT_COMMIT_REF ?? "";
-  return f(branchName.replace(/[^a-zA-Z0-9]/g, "-"));
+if (!reflectServer) {
+  throw new Error("Required env var VITE_REFLECT_SERVER is not defined");
 }
